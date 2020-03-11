b8b705c5d09bc1bd8b6b8082000fcb09c59d89f4
IT <anrl1014@naver.com>
2020-03-11 09:49:35 +0900

    Initial commit

---
+++ test.py
@@ -0,0 +1,96 @@
+walet = 99999
+print('유저가 가지고 있는돈:')
+print(walet)
+
+a = "life is too short, you need python"
+print(a[0:4])
+
+b = "20010331Rainy"
+# date = b[:8]
+# weather = b[8:]
+# print(date)
+# print(weather)
+#
+
+c = 12
+d = 1.2
+
+print(c, d)
+print(type(c))
+print(type(d))
+
+a = 20
+b = 3
+
+print(a / b)
+print(a // b)
+print(a * b)
+print(a - b)
+print(a % b)
+
+print("소소한\
+ 코딩\
+  교실")
+
+for i in range(10):
+    print(a * b)
+else:
+    print("바보")
+
+a = '행복한 코딩 교실'
+print(a.upper())
+print(len(a))
+
+print("현재 온도는 18도 입니다")
+print("i eat %s apply" % 3)
+print("i eat %s%% apply" % 4)
+
+# a = int(input())
+# b = int(input())
+# print(type(a))
+# print(type(b))
+# c = a+b
+
+print(str(c) + '두수의 합입니다')
+
+num = [1,2,3,4,5,['list','asd']]
+print(num[5][0])
+a = [1,2,3,4,5,['a','b','c'],6,7]
+b = [1,2,4]
+a [1:] = []
+
+
+num_pen = 4000
+num_penal = 3000
+
+a = [1,2,3,4]
+
+a[1] = (str(a[1]) + 'hi')
+
+print(a)
+
+a = [5,4,1,2,3]
+a.append(4)
+a.insert(3,100)
+print(a)
+a.sort()
+print(a)
+s = a.pop()
+print(s)
+print(a)
+a += [4,5]
+print(a)
+# a = int(input('연필의 구매 개수를 입력해주세요'))
+# b = int(input('볼팬의 구매 개수를 입력해주세요'))
+# c = ((num_penal * a) + (num_pen * b)) * 0.7
+# d = str(c)
+# e = d.index('.')
+# print(type(type(d)))
+# print('d의 클래스 타입은 '+str(type(d)))
+# print('구매하신 총 금액은 %d 원 입니다' % c)
+
+t1 = 1,2,3
+t1 += 4,5
+
+print(t1)
+
